0251679e37249b562dffc5e26f17d99be449abcc
ayushudaydubey <102001ayushdubey@gmail.com>
2024-11-26 15:26:08 +0530

    firstPromt

---
+++ script.js
@@ -0,0 +1,12 @@
+// console.log(document)
+
+console.log("hello world")
+console.log("hello world for everyone")
+
+//DOM
+// Maunuplating HTML css with the help of javascript dyanamically.
+
+// To auto refresh javascript  file we use  nodemon
+// npm install -g nodemon  --to insatll
+// nodemon script.js -- to start
+// CTRL +c -- to stop
